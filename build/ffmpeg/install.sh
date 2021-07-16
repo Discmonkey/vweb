f9b1501f4ccb476465d7826cdd44cc3e0e2202cf
max <maxgrinchenko@google.com>
2021-07-16 17:42:33 -0400

    Initial commit with default files and third_party dependencies

---
+++ build/ffmpeg/install.sh
@@ -0,0 +1,14 @@
+cd third_party/nv_codec_headers && sudo make install
+sudo apt-get install \
+  build-essential \
+  yasm \
+  cmake \
+  libtool \
+  libc6 \
+  libc6-dev \
+  unzip \
+  wget \
+  libnuma1 \
+  libnuma-dev
+cd -
+cd third_party/ffmpeg && ./configure --disable-static --enable-shared --enable-gpl --disable-doc && make -j 4
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/dismonkey/vweb
+
+go 1.14
+
+require (
+	github.com/giorgisio/goav v0.1.0
+)
