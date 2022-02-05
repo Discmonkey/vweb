b943bcd38b1719233f404a46359d76a4de9dbb34
max <maxgrinchenko@google.com>
2022-02-05 11:49:22 -0500

    Created nal package for decoding Nal Units

--- internal/nal/nal.go
+++ internal/nal/nal.go
@@ -1,53 +1,79 @@
 package nal
 
-type NalUnitType = int
-type Nal = byte
+type UnitType = int
+type Unit = byte
 
-func Type(n Nal) NalUnitType {
-
+func Type(n Unit) UnitType {
+	// the lowest 5 bits specify the nal type
+	return UnitType(n & 63)
 }
 
 const (
-	Unspecified NalUnitType = 0
-	Unspecified
-"Coded slice of a non-IDR picture
-slice_layer_without_partitioning_rbsp( )"
-"Coded slice data partition A
-slice_data_partition_a_layer_rbsp( )"
-"Coded slice data partition B
-slice_data_partition_b_layer_rbsp( )"
-"Coded slice data partition C
-slice_data_partition_c_layer_rbsp( )"
-"Coded slice of an IDR picture
-slice_layer_without_partitioning_rbsp( )"
-"Supplemental enhancement information (SEI)
-sei_rbsp( )"
-"Sequence parameter set
-seq_parameter_set_rbsp( )"
-"Picture parameter set
-pic_parameter_set_rbsp( )"
-"Access unit delimiter
-access_unit_delimiter_rbsp( )"
-"End of sequence
-end_of_seq_rbsp( )"
-"End of stream
-end_of_stream_rbsp( )"
-"Filler data
-filler_data_rbsp( )"
-"Sequence parameter set extension
-seq_parameter_set_extension_rbsp( )"
-"Prefix NAL unit
-prefix_nal_unit_rbsp( )"
-"Subset sequence parameter set
-subset_seq_parameter_set_rbsp( )"
-Reserved
-"Coded slice of an auxiliary coded picture without partitioning
-slice_layer_without_partitioning_rbsp( )"
-"Coded slice extension
-slice_layer_extension_rbsp( )"
-"Coded slice extension for depth view components
-slice_layer_extension_rbsp( )
-(specified in Annex I)"
-Reserved
-Unspecified
+	Unspecified                             UnitType = 0
+	NonIdrSliceLayerWithoutPartitioningRbsp          = 1
+	SliceDataPartitionALayerRbsp                     = 2
+	SliceDataPartitionBLayerRbsp                     = 3
+	SliceDataPartitionCLayerRbsp                     = 4
+	IdrSliceLayerWithoutPartitioningRbsp             = 5
+	SeiRbsp                                          = 6
+	SeqParameterSetRbsp                              = 7
+	PicParameterSetRbsp                              = 8
+	AccessUnitDelimiterRbsp                          = 9
+	EndOfSeqRbsp                                     = 10
+	EndOfStreamRbsp                                  = 11
+	FillerDataRbsp                                   = 12
+	SeqParameterSetExtensionRbsp                     = 13
+	PrefixNalUnitRbsp                                = 14
+	SubsetSeqParameterSetRbsp                        = 15
+	AuxSliceLayerWithoutPartitioningRbsp             = 19
+	SliceLayerExtensionRbsp                          = 20
+	DepthSliceLayerExtensionRbsp                     = 21
+	// Reserved actually takes multiple values
+	Reserved = 16
 )
+
+func toString(i UnitType) string {
+	value := "Unspecified"
+	switch i {
+	case NonIdrSliceLayerWithoutPartitioningRbsp:
+		value = "Coded slice of a non-IDR picture"
+	case SliceDataPartitionALayerRbsp:
+		value = "Coded slice data partition A"
+	case SliceDataPartitionBLayerRbsp:
+		value = "Coded slice data partition B"
+	case SliceDataPartitionCLayerRbsp:
+		value = "Coded slice data partition C"
+	case IdrSliceLayerWithoutPartitioningRbsp:
+		value = "Coded slice of an IDR picture"
+	case SeiRbsp:
+		value = "Supplemental enhancement information (SEI)"
+	case SeqParameterSetRbsp:
+		value = "Sequence parameter set"
+	case PicParameterSetRbsp:
+		value = "Picture parameter set"
+	case AccessUnitDelimiterRbsp:
+		value = "Access unit delimiter"
+	case EndOfSeqRbsp:
+		value = "End of sequence"
+	case EndOfStreamRbsp:
+		value = "End of stream"
+	case FillerDataRbsp:
+		value = "Filler data"
+	case SeqParameterSetExtensionRbsp:
+		value = "Sequence parameter set extension"
+	case PrefixNalUnitRbsp:
+		value = "Prefix NAL unit"
+	case SubsetSeqParameterSetRbsp:
+		value = "Subset sequence parameter set"
+	case AuxSliceLayerWithoutPartitioningRbsp:
+		value = "Coded slice of an auxiliary coded picture without partitioning"
+	case SliceLayerExtensionRbsp:
+		value = "Coded slice extension"
+	case DepthSliceLayerExtensionRbsp:
+		value = "Coded slice extension for depth view components"
+	case 16, 17, 18, 22, 23:
+		value = "Reserved"
+	}
+
+	return value
+}
