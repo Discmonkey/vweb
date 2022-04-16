fd67ecd4ed71383223ef9d0e0d941f522b1fbc8f
max <maxgrinchenko@google.com>
2022-04-16 13:30:21 -0400

    added endpoint for creating a source

--- pkg/endpoints/source/add.go
+++ pkg/endpoints/source/add.go
@@ -2,24 +2,62 @@ package source
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/discmonkey/vweb/pkg/android"
 	"github.com/discmonkey/vweb/pkg/endpoints/utils"
 	"github.com/discmonkey/vweb/pkg/swagger"
+	"github.com/discmonkey/vweb/pkg/video"
+	"log"
 	"net/http"
 	"sync"
 )
 
 type Manager struct {
-	m sync.Mutex
+	m       sync.Mutex
+	players map[string]video.Player
 }
 
-func post(res http.ResponseWriter, req *http.Request) {
+func post(m *Manager, res http.ResponseWriter, req *http.Request) {
 	var s swagger.Source
 	if utils.HttpNotOk(400, res, "decode error", json.NewDecoder(req.Body).Decode(&s)) {
 		return
 	}
 
+	m.m.Lock()
+	defer m.m.Unlock()
+
+	m.players[s.Name] = android.NewPlayer()
+
 }
 
-func get(w http.ResponseWriter, r *http.Request) {
+func get(m *Manager, w http.ResponseWriter) {
+	m.m.Lock()
+	defer m.m.Unlock()
 
+	sources := make([]swagger.Source, 0, len(m.players))
+	for k, v := range m.players {
+		sources = append(sources, swagger.Source{
+			Codec: v.Type(), Name: k,
+		})
+	}
+
+	err := json.NewEncoder(w).Encode(sources)
+	if err != nil {
+		log.Default().Println(err)
+	}
+}
+
+func Source(m *Manager) func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		switch req.Method {
+		case "GET":
+			get(m, w)
+		case "POST":
+			post(m, w, req)
+		default:
+			http.Error(w,
+				fmt.Sprintf("request %s not supported", req.Method),
+				404)
+		}
+	}
 }
