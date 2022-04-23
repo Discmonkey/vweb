3d9c84277afad04f9eff537f0ab1d8000898fdc5
max <maxgrinchenko@google.com>
2022-04-23 09:02:13 -0400

    factored out permission manager

--- rewinder/app/src/main/java/com/example/rewinder/ConnectActivity.kt
+++ rewinder/app/src/main/java/com/example/rewinder/ConnectActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rewinder.network.createSource
@@ -12,11 +13,16 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class ConnectActivity : AppCompatActivity() {
+
+    private var permissionManager = PermissionManager()
+
     @RequiresApi(32)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect)
-
+        if (!permissionManager.allPermissionsGranted(baseContext)) {
+            permissionManager.requestPermissions(this);
+        }
         val submit = findViewById<Button>(R.id.connect_button)
         val stream = findViewById<Button>(R.id.stream_button)
         val text = findViewById<EditText>(R.id.ip_field)
@@ -29,6 +35,21 @@ class ConnectActivity : AppCompatActivity() {
                     createSource(ip)
                 }
             }
+        }
+    }
+
+    @RequiresApi(32)
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PermissionManager.REQUEST_CODE_PERMISSIONS) {
+           if (!permissionManager.allPermissionsGranted(baseContext)) {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+           }
         }
     }
 }
--- rewinder/app/src/main/java/com/example/rewinder/network/Requests.kt
+++ rewinder/app/src/main/java/com/example/rewinder/network/Requests.kt
@@ -2,39 +2,57 @@ package com.example.rewinder.network
 
 import android.util.Log
 import io.swagger.server.models.Address
-import io.swagger.server.models.IP
-import io.swagger.server.models.Source
-import org.json.JSONObject
-import org.json.JSONTokener
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.result.Result
 import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
 
 // add source to server, and return the address to which we want to stream
-suspend fun createSource(address: String): Address {
-
-    runCatching {
-        val url = URL("http://192.168.1.10/source")
-        val postData = "{\"codec\":\"video/H264\"}"
-
-        val conn = url.openConnection()
-        conn.doOutput = true
-        conn.setRequestProperty("Content-Type", "application/json")
-        conn.setRequestProperty("Content-Length", postData.length.toString())
-
-        val stream = conn.getOutputStream();
-
-        DataOutputStream(stream).use {
-            it.writeBytes(postData)
+fun createSource(address: String): Address {
+    val bodyJson = """
+        { 
+            "codec" : "video/H264",
+            "name" : "max's phone"
         }
-        println("here")
-        BufferedReader(InputStreamReader(conn.getInputStream())).use { bf ->
-            var line: String?
-            while (bf.readLine().also { line = it } != null) {
-                println(line)
+    """
+    val httpAsync = "http://192.168.1.10:3000/source"
+        .httpPost()
+        .body(bodyJson)
+        .responseString { request, response, result ->
+            when (result) {
+                is Result.Failure -> {
+                    val ex = result.getException()
+                    println(ex)
+                }
+                is Result.Success -> {
+                    val data = result.get()
+                    println(data)
+                }
             }
         }
-    }
+//    runCatching {
+//        val url = URL("http://192.168.1.10:3000/source")
+//        val postData = "{\"codec\":\"video/H264\"}"
+//
+//        val conn = url.openConnection()
+//        conn.doOutput = true
+//        conn.setRequestProperty("Content-Type", "application/json")
+//        conn.setRequestProperty("Content-Length", postData.length.toString())
+//
+//        val stream = conn.getOutputStream();
+//        println("here")
+//        DataOutputStream(stream).use {
+//            it.writeBytes(postData)
+//        }
+//        println("here")
+//        BufferedReader(InputStreamReader(conn.getInputStream())).use { bf ->
+//            var line: String?
+//            while (bf.readLine().also { line = it } != null) {
+//                println(line)
+//            }
+//        }
+//    }
 
     return Address("hello", 3000)
 }
